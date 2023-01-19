06beeea737e49c6a388fd2091379dac9e494e6e3
Wilven Sabio <122994935+xaxa09@users.noreply.github.com>
2023-01-19 22:11:45 +0800

    Add files via upload

---
+++ src/resumeData.js
@@ -0,0 +1,51 @@
+let resumeData = {
+    "name": "Wilven Klenn L. Sabio",
+    "role": "BSCS Student ",
+    "skypeid": "Your skypeid",
+    "roleDescription": "Student Assistant and Computer Technician of Department of Computer Science Office",
+    "socialLinks":[
+        {
+          "name":"Facebook",
+          "url":"https://www.facebook.com/wilven.sabio.73/",
+          "className":"fa fa-facebook"
+        },
+        {
+          "name":"Github",
+          "url":"https://github.com/xaxa09",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I'm Wilven Klenn L. Sabio a Computer Science student of NEMSU.",
+    "address":"P-4 Payasan, Lianga, Surigao del Sur",
+    "website":"https://github.com/xaxa09",
+    "education":[
+      {
+        "UniversityName":"NORTH EASTERN MINDANAO STATE UNIVERSITY - Lianga Campus",
+        "Specialization":"Computer Technician",
+        "MonthOfPassing":"Bot lang wala pamana klaro ani murag gugma lage",
+        "YearOfPassing":"Kung tuig lang basihan bot lang basin karun puhon na tuig oh sunod.",
+        "Achievements":"Passed the Thesis 1 and for the Thesis 2 ambot lang duha-duha pa."
+      },
+      {
+        "UniversityName":"NORTH EASTERN MINDANAO STATE UNIVERSITY - Lianga Campus",
+        "Specialization":"Computer Technician",
+        "MonthOfPassing":"Bot lang wala pamana klaro ani murag gugma lage",
+        "YearOfPassing":"Kung tuig lang basihan bot lang basin karun puhon na tuig oh sunod.",
+        "Achievements":"Passed the Thesis 1 and for the Thesis 2 ambot lang duha-duha pa."
+      }
+    ],
+    "skillsDescription":"PROGRAMMING",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+  }
+  
+  export default resumeData
